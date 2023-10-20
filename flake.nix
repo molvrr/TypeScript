0674dcbd06bdf80a579fea0a7cb7709d72776de3
Mateus Cruz <mateuscolvr@gmail.com>
2023-10-20 19:40:27 -0300

    Increase tail count limit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages.typescript-language-server
+            nodePackages.vscode-langservers-extracted
+            nodejs_20
+          ];
+        };
+      });
+}
